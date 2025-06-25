50316cd4d072cd861344de2134dbc65887bac574
DavidZahn <David-Zahn@hotmail.com>
2025-06-25 13:23:50 +0200

    test html for code 4 and typescript

---
+++ Task_4_Coding/Test.js
@@ -0,0 +1,35 @@
+"use strict";
+// console.log("Hello World");
+// let v= "1";
+// v= v+1;
+// console.log(v)
+// let v: number =1;
+// console.log(v)
+// let greet: string ="Hello";
+// console.log(greet)
+// let a = [7, true, "Hallo"]
+// console.log(a[1])
+// let s = {"zahl": 7, "wahr": true, text: "Hallo"}
+// console.log(s["zahl"])
+// let s[4] = [101, 102];
+// interface Student {
+//     name: string;
+//     matrikel: number;
+//     grades: {[module: string]: number};
+// }
+// let students: Student[] = [];
+// students.push({name: "Big Brain", matrikel: 123456, grades: {"EIA1": 1.3, "EIA2": 1.0}});
+//  function isDivisible(_dividend: number, _divisor: number): boolean {
+//     let result: boolean = (_dividend % _divisor == 0);
+//     return result;
+//   }
+// console.log(isDivisible(4, 3)); // false
+// console.log(isDivisible(4, 2)); // true
+// console.log(isDivisible(10, 0)); // Fehler-Message und false
+let v1 = 10;
+let v2 = v1; // v2 bekommt den Wert von v1 kopiert
+// Wert von v1 ändern
+v1 = 20;
+// Ausgabe beider Variablen
+console.log("v1:", v1); // 20
+console.log("v2:", v2); // 10
