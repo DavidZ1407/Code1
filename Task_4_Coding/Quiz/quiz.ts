312fda7fdd12acd11934bd7190abdd78586762d8
DavidZahn <David-Zahn@hotmail.com>
2025-07-01 14:32:10 +0200

    implementet arrays correct and promt

--- Task_4_Coding/Quiz/quiz.ts
+++ Task_4_Coding/Quiz/quiz.ts
@@ -1,37 +1,31 @@
-namespace QuizGame{
+namespace QuizGame {
 
 
-let answer: string = prompt(data["singleChoice"]["question1"][0]) || "";
-checkQuestion(data["singleChoice"]["question1"][0], answer)
+    checkQuestion(data["singleChoice"]["question1"][1], prompt(data["singleChoice"]["question1"][0]) || "", data["singleChoice"]["question1"][0])
 
-console.log("(1) Runtime Error")
-console.log("(2) Compiletime Error")
-console.log("(3) Didacticle Error")
-console.log("(4) Designtime Error")
-answer = prompt(data["multipleChoice"]["question1"][0]) || "";
 
-checkQuestion(data["multipleChoice"]["question1"][0], answer)
+    console.log("(1) Runtime Error")
+    console.log("(2) Compiletime Error")
+    console.log("(3) Didacticle Error")
+    console.log("(4) Designtime Error")
+    checkQuestion(data["multipleChoice"]["question1"][1], prompt(data["multipleChoice"]["question1"][0]) || "", data["multipleChoice"]["question1"][0])
+    checkQuestion(data["textEntry"]["question1"][1], prompt(data["textEntry"]["question1"][0]) || "", data["textEntry"]["question1"][0])
+    checkQuestion(data["estimation"]["question1"][1], prompt(data["estimation"]["question1"][0]) || "", data["estimation"]["question1"][0])
 
-answer = prompt(data["textEntry"]["question1"][0]) || "";
-checkQuestion(data["textEntry"]["question1"][0], answer)
+    alert("You have won the game!");
 
-answer = prompt(data["estimation"]["question1"][0]) || "";
-checkQuestion(data["estimation"]["question1"][0], answer)
 
-alert("You have won the game!")
+    function checkQuestion(_rightAnswer: string, _answer: string, _question: string) {
 
-function checkQuestion(_question: string, _answer: string){
+        if (_rightAnswer.toLowerCase() === _answer.toLowerCase()) {
+            alert("Correct");
+            return;
+        }
 
-    if (_question.toLowerCase == _answer.toLowerCase){
-        alert("Correct")
-        return;
+        else {
+            alert("Wrong Answer");
+
+            return checkQuestion(_rightAnswer, prompt(_question) || "", _question)
+        }
     }
-
-    else{
-        alert("Wrong Answer")
-        return checkQuestion(_question, _answer)
-    }
-}
-
-
 }
