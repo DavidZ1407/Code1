f86849a1a90927bc73831999ee7879600005d550
DavidZahn <David-Zahn@hotmail.com>
2025-07-10 14:25:33 +0200

    changed linter mistake for control ts

--- Task_4_Coding/control/control.ts
+++ Task_4_Coding/control/control.ts
@@ -17,10 +17,10 @@ namespace Control {
     const keysPressed: { [key: string]: boolean } = {};
     let mousePosition: Vector2 = { x: 0, y: 0 };
 
-    
+
     window.addEventListener("load", handleLoad);
 
-    function handleLoad() {
+    function handleLoad(): void {
         tank1 = {
             element: createTank("red"),
             position: { x: 100, y: 100 }
@@ -34,28 +34,28 @@ namespace Control {
         document.body.appendChild(tank1.element);
         document.body.appendChild(tank2.element);
 
-        window.addEventListener("keydown", (e) => keysPressed[e.key] = true);
-        window.addEventListener("keyup", (e) => keysPressed[e.key] = false);
+        window.addEventListener("keydown", (_e) => keysPressed[_e.key] = true);
+        window.addEventListener("keyup", (_e) => keysPressed[_e.key] = false);
 
-        window.addEventListener("mousemove", (e) => {
-            mousePosition = { x: e.clientX - 20, y: e.clientY - 20 };
+        window.addEventListener("mousemove", (_e) => {
+            mousePosition = { x: _e.clientX - 20, y: _e.clientY - 20 };
         });
 
         requestAnimationFrame(update);
     }
 
-    function createTank(color: string): HTMLDivElement {
-        const tank = document.createElement("div");
+    function createTank(_color: string): HTMLDivElement {
+        const tank: HTMLDivElement = document.createElement("div");
         tank.style.position = "absolute";
         tank.style.width = "40px";
         tank.style.height = "40px";
-        tank.style.background = color;
+        tank.style.background = _color;
         tank.style.borderRadius = "4px";
         return tank;
     }
 
-    function update() {
-        const speed = 5;
+    function update(): void {
+        const speed: number = 5;
 
         if (keysPressed["ArrowUp"]) tank1.position.y -= speed;
         if (keysPressed["ArrowDown"]) tank1.position.y += speed;
@@ -71,93 +71,93 @@ namespace Control {
         requestAnimationFrame(update);
     }
 
-    function updateTank(tank: Tank) {
-        tank.element.style.left = tank.position.x + "px";
-        tank.element.style.top = tank.position.y + "px";
+    function updateTank(_tank: Tank): void {
+        _tank.element.style.left = _tank.position.x + "px";
+        _tank.element.style.top = _tank.position.y + "px";
     }
 
 
 
-     // ================================
+    // ================================
     // Events (Immediate Reaction)
     // ================================
 
 
-        // type Vector2 = { x: number; y: number };
+    // type Vector2 = { x: number; y: number };
 
-        // type Tank = {
-        //     element: HTMLDivElement;
-        //     position: Vector2;
-        // };
+    // type Tank = {
+    //     element: HTMLDivElement;
+    //     position: Vector2;
+    // };
 
-        // let tank1: Tank;
-        // let tank2: Tank;
+    // let tank1: Tank;
+    // let tank2: Tank;
 
-        // window.addEventListener("load", handleLoad);
+    // window.addEventListener("load", handleLoad);
 
-        // function handleLoad() {
-        //     // Tank 1 (keyboard)
-        //     tank1 = {
-        //         element: createTank("red"),
-        //         position: { x: 100, y: 100 }
-        //     };
+    // function handleLoad(): void {
+    //     // Tank 1 (keyboard)
+    //     tank1 = {
+    //         element: createTank("red"),
+    //         position: { x: 100, y: 100 }
+    //     };
 
-        //     // Tank 2 (mouse)
-        //     tank2 = {
-        //         element: createTank("blue"),
-        //         position: { x: 200, y: 200 }
-        //     };
+    //     // Tank 2 (mouse)
+    //     tank2 = {
+    //         element: createTank("blue"),
+    //         position: { x: 200, y: 200 }
+    //     };
 
-        //     document.body.appendChild(tank1.element);
-        //     document.body.appendChild(tank2.element);
+    //     document.body.appendChild(tank1.element);
+    //     document.body.appendChild(tank2.element);
 
-        //     window.addEventListener("keydown", handleKeyDown);
-        //     window.addEventListener("mousemove", handleMouseMove);
+    //     window.addEventListener("keydown", handleKeyDown);
+    //     window.addEventListener("mousemove", handleMouseMove);
 
-        //     requestAnimationFrame(update);
-        // }
+    //     requestAnimationFrame(update);
+    // }
 
-        // function createTank(color: string): HTMLDivElement {
-        //     const tank = document.createElement("div");
-        //     tank.style.position = "absolute";
-        //     tank.style.width = "40px";
-        //     tank.style.height = "40px";
-        //     tank.style.background = color;
-        //     tank.style.borderRadius = "4px";
-        //     return tank;
-        // }
+    // function createTank(_color: string): HTMLDivElement {
+    //     const tank: HTMLDivElement = document.createElement("div");
+    //     tank.style.position = "absolute";
+    //     tank.style.width = "40px";
+    //     tank.style.height = "40px";
+    //     tank.style.background = _color;
+    //     tank.style.borderRadius = "4px";
+    //     return tank;
+    // }
 
-        // function handleKeyDown(event: KeyboardEvent) {
-        //     const speed = 5;
-        //     switch (event.key) {
-        //         case "ArrowUp":
-        //             tank1.position.y -= speed;
-        //             break;
-        //         case "ArrowDown":
-        //             tank1.position.y += speed;
-        //             break;
-        //         case "ArrowLeft":
-        //             tank1.position.x -= speed;
-        //             break;
-        //         case "ArrowRight":
-        //             tank1.position.x += speed;
-        //             break;
-        //     }
-        // }
+    // function handleKeyDown(_event: KeyboardEvent): void {
+    //     const speed: number= 5;
+    //     switch (_event.key) {
+    //         case "ArrowUp":
+    //             tank1.position.y -= speed;
+    //             break;
+    //         case "ArrowDown":
+    //             tank1.position.y += speed;
+    //             break;
+    //         case "ArrowLeft":
+    //             tank1.position.x -= speed;
+    //             break;
+    //         case "ArrowRight":
+    //             tank1.position.x += speed;
+    //             break;
+    //     }
+    // }
 
-        // function handleMouseMove(event: MouseEvent) {
-        //     tank2.position.x = event.clientX - 20;
-        //     tank2.position.y = event.clientY - 20;
-        // }
+    // function handleMouseMove(_event: MouseEvent):void {
+    //     tank2.position.x = _event.clientX - 20;
+    //     tank2.position.y = _event.clientY - 20;
+    // }
 
-        // function update() {
-        //     updateTank(tank1);
-        //     updateTank(tank2);
-        //     requestAnimationFrame(update);
-        // }
+    // function update(): void {
+    //     updateTank(tank1);
+    //     updateTank(tank2);
+    //     requestAnimationFrame(update);
+    // }
 
-        // function updateTank(tank: Tank) {
-        //     tank.element.style.left = tank.position.x + "px";
-        //     tank.element.style.top = tank.position.y + "px";
-        // }
+    // function updateTank(_tank: Tank): void {
+    //     _tank.element.style.left = _tank.position.x + "px";
+    //     _tank.element.style.top = _tank.position.y + "px";
+    // }
 }
